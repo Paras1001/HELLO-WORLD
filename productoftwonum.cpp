270fd04aa01fb88bfacd1a670540a5482589857a
Paras1001 <67842102+Paras1001@users.noreply.github.com>
2020-07-10 19:22:48 +0530

    Update productoftwonum.cpp

--- productoftwonum.cpp
+++ productoftwonum.cpp
@@ -5,6 +5,7 @@ int main()
 	double firstno , secondno , product;
 	cout<<"enter two no.";
 	cin>>firstno>>secondno;
+	//product of two numbers will be displayed
 	product= firstno * secondno;
 	cout<<"product ="<<product;
 	return 0;
