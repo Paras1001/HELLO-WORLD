0c16c1987a3e21dd71da63a441a75689b163e59d
Paras1001 <67842102+Paras1001@users.noreply.github.com>
2020-07-10 19:21:26 +0530

    Add files via upload
    To find the product of two numbers

---
+++ productoftwonum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	double firstno , secondno , product;
+	cout<<"enter two no.";
+	cin>>firstno>>secondno;
+	product= firstno * secondno;
+	cout<<"product ="<<product;
+	return 0;
+}
