e69b1f349fee0ae6d0b4fbca32bdcefa2e81c6c6
Paras1001 <67842102+Paras1001@users.noreply.github.com>
2020-07-17 00:30:36 +0530

    Update calculator.cpp
    THE CALCULATOR INCLUDES ADDITION , SUBTRACTION , MULTIPLICATION , DIVISION , SQUARING AND SQUARE ROOT
    USING SWITCH - CASE STATEMENTS.

--- calculator.cpp
+++ calculator.cpp
@@ -1,53 +1,94 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
-
 main()
 {
-	double num1 , num2 , res;
-	int op;
-	cout<<"calculator";
-	cout<<"\n1. addition";
-	cout<<"\n2. subtraction";
-	cout<<"\n3. multiplication";
-	cout<<"\n4. division";
-	cout<<"\n\tEnter first no. ";
-	
-	cin>>num1;
-	cout<<"\n\tEnter second no.";
+	int num1 , num2 , num3 , num4 , op ,  res1 , res2 , res;
+	cout<<"1) ADDITION \n2) SUBTRACTION \n3) MULTIPY \n4) DIVISION \n5) SQUARE \n6) SQUARE ROOT ";               //ADDITION
+	cout<<"\n\tENTER FIRST NUMBER : ";                                                                           //SUBTRACTION
+	cin>>num1;                                                                                                   //MULTIPLY
+	cout<<"\nSELECT OPERATOR : ";                                                                                //DIVIDE
+	cin>>op;                                                                                                     //SQUARING
+	switch(op)                                                                                                   //SQUARE ROOT
+	{
+		case 5:
+			res=num1*num1;
+			cout<<"SQUARE :"<<res;
+			return 0;
+		case 6:
+		    res=pow(num1 , 0.5);
+		    cout<<"SQUARE ROOT :"<<res;
+		    return 0;
+	}
+	cout<<"\n\tENTER SECOND NUMBER :";
 	cin>>num2;
-	cin>>op;
 	switch(op)
 	{
 		case 1:
-			//addition
-			res = num1+num2;
-			cout<<res;
+			res1=num1+num2;
+			cout<<res1;
 			break;
 		case 2:
-			//subraction
-			res = num1-num2;
-			cout<<res;
+		    res1=num1-num2;
+			cout<<res1;
 			break;
 		case 3:
-			//multiplication
-			res = num1*num2;
-			cout<<res;
+		    res1=num1*num2;
+			cout<<res1;
 			break;
 		case 4:
-			//division
-			res = num1/num2;
-			cout<<res;
+		    res1=num1/num2;
+			cout<<res1;
+			break;			
+	}
+	cout<<"\nSELECT OPERATOR : ";
+	cin>>op;
+	cout<<"\n\tENTER THIRD NUMBER :";
+	cin>>num3;
+	switch(op)
+	{
+	
+		case 1:
+			res2=res1+num3;
+			cout<<res2;
 			break;
-		default:
-			cout<<"invalid input";
+		case 2:
+		    res2=res1-num3;
+			cout<<res2;
 			break;
-		
-		
-			
-		   	
-			        
-    }
+		case 3:
+		    res2=res1*num3;
+			cout<<res2;
+			break;
+		case 4:
+		    res2=res1/num3;
+			cout<<res2;
+			break;
+	}
+	cout<<"\nSELECT OPERATOR : ";
+	cin>>op;
+	cout<<"\n\tENTER FOURTH NUMBER :";
+	cin>>num4;
+	switch(op)
+	{
+	
+		case 1:
+			res=res2+num4;
+			cout<<"FINAL RESULT : "<<res;
+			break;
+		case 2:
+		    res=res2-num4;
+			cout<<"FINAL RESULT : "<<res;
+			break;
+		case 3:
+		    res=res2*num4;
+			cout<<"FINAL RESULT : "<<res;
+			break;
+		case 4:
+		    res=res2/num4;
+			cout<<"FINAL RESULT : "<<res;
+			break;
+	}
 	return 0;
-	
-	
+		
 }
