75597ec03bbe1e10047493668d15ab7b51cee2b0
Paras1001 <67842102+Paras1001@users.noreply.github.com>
2020-08-02 05:58:57 +0530

    Update roots of quadratic equations.cpp

--- roots of quadratic equations.cpp
+++ roots of quadratic equations.cpp
@@ -15,16 +15,16 @@ int main()
 		cout<<"real roots"<<endl;
 		d4 = sqrt(d);
 		x1 = (-b+d4)/(2*a);
-	    x2 = (-b-d4)/(2*a);
-	    cout<<"\nThe roots of the equation is : "<<x1<<" , "<<x2<<endl;
+	        x2 = (-b-d4)/(2*a);
+	        cout<<"\nThe roots of the equation is : "<<x1<<" , "<<x2<<endl;
 	}
 	if(d==0)
 	{
 		cout<<"equal roots"<<endl; 
 		d4 = sqrt(d);
 		x1 = (-b+d2)/(2*a);
-	    x2 = (-b-d2)/(2*a);
-	    cout<<"\nThe roots of the equation is : "<<x1<<" , "<<x2<<endl; 
+	        x2 = (-b-d2)/(2*a);
+	        cout<<"\nThe roots of the equation is : "<<x1<<" , "<<x2<<endl; 
 	}
 	if(d<0)
 	{
@@ -33,7 +33,7 @@ int main()
 		d2=sqrt(d3);
 		
 		cout<<"\n x1 = (-"<<b<<"+"<<d2<<"i)"<<"/"<<2*a;
-    	cout<<"\n x2 = (-"<<b<<"-"<<d2<<"i)"<<"/"<<2*a;       
+    	        cout<<"\n x2 = (-"<<b<<"-"<<d2<<"i)"<<"/"<<2*a;       
 	}
 	return 0;
 }
