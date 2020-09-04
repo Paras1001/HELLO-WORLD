12abfe19845bd66a886fc2d6cc3b82a95b1f76ea
Paras <67842102+Paras1001@users.noreply.github.com>
2020-09-05 01:51:50 +0530

    Update what do i eat.cpp

--- what do i eat.cpp
+++ what do i eat.cpp
@@ -1,19 +1,18 @@
 #include<stdio.h>
-
 main()
 {
 	int op,price;
-	char item[9];
+start :
 	printf("Enter your choice (1-5) : ");
-	scanf("%d",&op);
+	scanf("%d",&op);                 //Enter choice from 1,2,3,4,5
 	switch(op)
 	{
 		case 1:
 			price=129;
 			printf("\nFood item - Burger");
-        	printf("\nPrice - %d",price);
+        	        printf("\nPrice - %d",price);
 			break;
-      	case 2:
+         	case 2:
 			printf("\nFood item - Fries");
 			price = 99;
 			printf("\nPrice - %d",price);
@@ -32,6 +31,8 @@ main()
 			printf("\nFood item - Sandwich");
 			price = 149;
 			printf("\nPrice - %d",price);
-			break;			
+			break;	
+		case 6:                       
+			goto start;		 //you will go to start and again fill the correct option
 	}
 }
